aa6d3b52ee1fb0d3cef21ff2b9b5276c86e53e4b
graceatmubea <grace.chan@mubea.com>
2025-06-24 18:29:44 +0000

    0

---
+++ pgTitle.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+#Startup Page
+
+st.title("CI-QUALITY WEEK GAMES")
+
+if st.button(label="Escape Room", key=None, help=None, type="primary", icon=None,
+            disabled=False, use_container_width=True):
+    st.switch_page("pg1.py")
+if st.button(label="Jeopardy", key=None, help=None, type="primary", icon=None,
+            disabled=False, use_container_width=True):
+        st.switch_page("pg2.py")
+if st.button(label="Root Cause Analysis", key=None, help=None, type="primary", icon=None,
+            disabled=False, use_container_width=True):
+        st.switch_page("pg3.py")
