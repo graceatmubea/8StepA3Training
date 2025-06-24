2e829aa95a5be6e8d8c8624fa565af017e4ca115
graceatmubea <grace.chan@mubea.com>
2025-06-24 18:32:40 +0000

    00

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 
-
-pg = st.navigation([
-    st.Page("pgTitle.py", title="Title", icon=":material/favorite:"),
-    st.Page("pg1.py", title="Batch", icon="🔥"),
-    st.Page("pg2.py", title="Single", icon="🔥"),
-    st.Page("pg3.py", title="Single", icon="🔥")
-], position='hidden')
-pg.run()
+st.title("🎈 My new app")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
